2384063413efd5fbb109b45a2356963515fcdfb9
Sam Albers <sam.albers@gov.bc.ca>
2019-09-30 14:10:41 -0700

    render figs at top level

--- bin/chunk-options.R
+++ bin/chunk-options.R
@@ -28,7 +28,7 @@ knitr_fig_path <- function(prefix) {
 ## `fig.process` option.
 
 opts_chunk$set(tidy = FALSE, results = "markup", comment = NA,
-               fig.align = "center", fig.path = "../fig/rmd-",
+               fig.align = "center", fig.path = "fig/rmd-",
                #fig.process = fix_fig_path,
                fig.width = 8.5, fig.height = 8.5,
                fig.retina = 2)
